def95607aca497498b326637134eca44ffc67585
WilliamOSoares <williamoliveirafsa@outlook.com>
2021-08-31 14:32:49 -0300

    Mudando o nome

--- AnalisadorLexico.py
+++ AnalisadorLexico.py
@@ -682,7 +682,7 @@ else:
                         break
                 countLinha+=1
             output(0,"ERRO","")
-        entrada.close
+            entrada.close()
         countArq+=1
         countLinha=1
         buffer=""
